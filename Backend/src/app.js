8863483a0208f7d951f1409a8cbbc8af32206413
Megha <meghagusain03@gmail.com>
2026-04-06 01:07:16 +0530

    Added Routers for registering user

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,9 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
-
+const authRoutes = require('./routes/auth.routes');
 const app = express();
 app.use(express.json());
 app.use(cookieParser());// To set data in the cookies
 
+app.use('/api/auth', authRoutes);
 module.exports = app;
