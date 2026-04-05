518c2e2d521ea0cfad73eb8ef4670d5ab926311a
Megha <meghagusain03@gmail.com>
2026-04-06 01:08:23 +0530

    Added controller for register logic

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,28 @@
+const userModel = require('../models/user.model');
+const jsonwebtoken = require('jsonwebtoken');
+async function registerUser(req, res) {
+    try {
+        const { username, email, password , role = "user" } = req.body;
+        const existingUser = await userModel.findOne({ 
+            $or: [{ email }, { username }] });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        const user = await userModel.create({ username, email, password, role });
+
+        const token = jsonwebtoken.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET);
+        res.cookie('token', token);
+        res.status(201).json({ message: 'User registered successfully', user:{
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        } });
+    } catch (error) {
+        res.status(500).json({ message: 'Error registering user', error: error.message });
+    }
+}
+
+module.exports = {
+    registerUser,
+};
